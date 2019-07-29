4fce4b92bab6c97115684500db21653420eed33a
Omer Harel <omerha@mta.ac.il>
2019-07-29 15:22:36 +0300

    init

---
+++ C19 Ex01 Omer 204059331 Andrey 321082513.sln/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace C19_Ex01_Omer_204059331_Andrey_321082513.sln
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
